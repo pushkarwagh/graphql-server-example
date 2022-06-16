66377a1e50e8ed2a11d194f3c344289c7acdb0b9
pushkarwagh <puwagh@bestpeers.com>
2022-06-16 15:36:37 +0530

    changes in schema

--- resolvers.js
+++ resolvers.js
@@ -1,4 +1,13 @@
+const db = require("./db")
+
 const Query = {
-    test: () => 'Test Success, GraphQL server is up & running !!'
+    test: () => 'Test Success, GraphQL server is up & running !!',
+    students: ()=> db.students.list()
  }
- module.exports = {Query}
+
+ const Students = {
+     college: (root) => {
+         return db.colleges.get(root.collegeId)
+     }
+ }
+ module.exports = {Query, Students}
